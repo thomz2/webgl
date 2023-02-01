a7425e05c290008ec694c6732002da02a9d6b3e4
Thomaz <82160387+thomz2@users.noreply.github.com>
2023-01-31 23:46:01 -0300

    Update and rename carro.js to cenario_principal.js

--- src/js/cenario_principal.js
+++ src/js/cenario_principal.js
@@ -8,6 +8,7 @@ console.log(Track)
 
 import * as CANNON from 'cannon-es';
 import CannonDebugger from 'cannon-es-debugger';
+import { Carro } from './classes/Car1';
 
 //Shaders
 const VS = `
@@ -25,14 +26,6 @@ varying vec4 pos;
 varying vec3 v_normal;
 void main() {
     gl_FragColor = (vec4(v_normal*v_normal, 1.0)+ pos )* (vec4(v_normal*v_normal, 1.0) - pos)*((vec4(v_normal*v_normal, 1.0)+ pos )* (vec4(v_normal*v_normal, 1.0) - pos)) + vec4(0.0,0.0,0.0,1.0) ;
-}
-`;
-
-const FS2 = `
-varying vec4 pos;
-varying vec3 v_normal;
-void main() {
-    gl_FragColor = (vec4(v_normal, 1.0)*pos) ;
 }
 `;
 
@@ -62,15 +55,9 @@ const options = {
     sphereMass: 1,
     wheelForce: 10,
     wheelSteer: Math.PI / 8,
-    debug: false
+    debug: false,
+    tamanhoCarro: 1
 };
-
-function attOptions() {
-    boxBody.mass = options.boxMass;
-    sphereBody.mass = options.sphereMass;
-    maxForce = options.wheelForce;
-    maxSteerVal = options.wheelSteer;
-}
 
 gui = new dat.GUI();
 
@@ -78,6 +65,7 @@ gui.add(options, 'boxMass', 0.1, 2);
 gui.add(options, 'sphereMass', 0.1, 2);
 gui.add(options, 'wheelForce', 5, 100);
 gui.add(options, 'wheelSteer', Math.PI / 16, Math.PI / 2);
+gui.add(options, 'tamanhoCarro', 0.1, 1.5);
 gui.add({"Mudar Câmera": () => camera.lockOn = !camera.lockOn}, 'Mudar Câmera')
 gui.add({"Modo debug": () => options.debug = !options.debug}, 'Modo debug')
 
@@ -127,23 +115,7 @@ Race.buildings.map(construcao =>{
     ))
 });
 
-//Malhas do Threejs: malhas do carro
-const criarMaterial = () => new THREE.ShaderMaterial({uniforms:{}, vertexShader:VS, fragmentShader:FS2});
-
-const carro = {
-    chassis:  new THREE.Mesh(new THREE.BoxGeometry(8, 1, 4), criarMaterial()),
-    roda1: new THREE.Mesh(new THREE.SphereGeometry(1), criarMaterial()),
-    roda2: new THREE.Mesh(new THREE.SphereGeometry(1), criarMaterial()),
-    roda3: new THREE.Mesh(new THREE.SphereGeometry(1), criarMaterial()),
-    roda4: new THREE.Mesh(new THREE.SphereGeometry(1), criarMaterial())
-}
-
-scene.add(carro.chassis);
-scene.add(carro.roda1);
-scene.add(carro.roda2);
-scene.add(carro.roda3);
-scene.add(carro.roda4);
-
+const carro = new Carro();
 
 const world = new CANNON.World({
     gravity: new CANNON.Vec3(0, -9.81, 0)
@@ -205,46 +177,20 @@ const groundSphereContactMat = new CANNON.ContactMaterial(
 
 world.addContactMaterial(groundSphereContactMat);
 
-
-const carBody = new CANNON.Body({
-    mass: 5,
-    position: new CANNON.Vec3(0, 6, 0),
-    shape: new CANNON.Box(new CANNON.Vec3(4, 0.5, 2))
-});
-
-const vehicle = new CANNON.RigidVehicle({
-    chassisBody:carBody
-});
-
-const positions = [
-    new CANNON.Vec3(-2, 0, 2,5),
-    new CANNON.Vec3(-2, 0, -2,5),
-    new CANNON.Vec3(2, 0, 2,5),
-    new CANNON.Vec3(2, 0, -2,5),
-];
-
-for (var i = 0; i < 4; ++i) {
-    vehicle.addWheel({
-        body: new CANNON.Body({mass: 1, material: new CANNON.Material('wheel')}),
-        position: positions[i],
-        axis: new CANNON.Vec3(0, 0, 1),
-        direction: new CANNON.Vec3(0, -1, 0)
-    });
-    vehicle.wheelBodies[i].addShape(new CANNON.Sphere(1));
-    vehicle.wheelBodies[i].angularDamping = 0.4;
-}
-
-vehicle.addToWorld(world);
-
 var maxForce = 40;
 var maxSteerVal = Math.PI / 8;
 
+const vehicle = carro.vehicle;
 
+carro.addToScene(scene);
+carro.addToWorld(world);
+
+// adicionando corpos das arvores pelo CANNON
 Race.trees.map(arvore => {
 
     const tree = new CANNON.Body({
         mass: 0,
-        position: new CANNON.Vec3(...arvore.position).vadd(new CANNON.Vec3(0,2,0)),
+        position: new CANNON.Vec3(...arvore.position).vadd(new CANNON.Vec3(0,2,0)), // somando vetor
         material: boxPhysMat
     });
 
@@ -260,7 +206,6 @@ Race.trees.map(arvore => {
         new CANNON.Quaternion()
     );
 
-
     world.addBody(tree);
 });
 
@@ -269,11 +214,11 @@ class ThirdPersonCamera {
     constructor(params){
         this._params = params;
         this._camera = params.camera;
-
+        
         this._currentPosition = new THREE.Vector3();
         this._currentLookAt = new THREE.Vector3();
     }
-
+    
     _CalculateIdealOffset(){
         const idealOffset = new THREE.Vector3(...this._params.position);
         idealOffset.applyQuaternion(this._params.target.quaternion);
@@ -306,7 +251,7 @@ class ThirdPersonCamera {
 //LookAt é onde a camera aponta em relação ao alvo
 const thirdPerson = new ThirdPersonCamera({
     camera: camera,
-    target: carro.chassis,
+    target: carro.carroMesh.chassis,
     position: [20, 5, 0],
     lookAt: [-25,0,0]
 });
@@ -342,7 +287,7 @@ document.addEventListener('keydown', (e) => {
             vehicle.setSteeringValue(-maxSteerVal, 0);
             vehicle.setSteeringValue(-maxSteerVal, 1);
             break;
-    }
+        }
 });
 
 document.addEventListener('keyup', (e) => {
@@ -353,16 +298,16 @@ document.addEventListener('keyup', (e) => {
             vehicle.setWheelForce(0, 0);
             vehicle.setWheelForce(0, 1);
             break;
-        
+            
         case 'S':
         case 's':
         case 'ArrowDown':
             vehicle.setWheelForce(0, 0);
             vehicle.setWheelForce(0, 1);
             break;
-    
+            
         case 'A':
-        case 'a':
+            case 'a':
         case 'ArrowLeft':
             vehicle.setSteeringValue(0, 0);
             vehicle.setSteeringValue(0, 1);
@@ -383,21 +328,21 @@ document.addEventListener('keyup', (e) => {
 
 const timeStep = 1 / 60;
 
+
+function attOptions() {
+    boxBody.mass = options.boxMass;
+    sphereBody.mass = options.sphereMass;
+    maxForce = options.wheelForce;
+    maxSteerVal = options.wheelSteer;
+    carro.setTamanho(options.tamanhoCarro);
+}
+
 function animate() {
     world.step(timeStep);
-
+    
     if(options.debug) cannonDebugger.update()
 
-    carro.chassis.position.copy(vehicle.chassisBody.position);
-    carro.roda1.position.copy(vehicle.wheelBodies[0].position);
-    carro.roda2.position.copy(vehicle.wheelBodies[1].position);
-    carro.roda3.position.copy(vehicle.wheelBodies[2].position);
-    carro.roda4.position.copy(vehicle.wheelBodies[3].position);
-    carro.chassis.quaternion.copy(vehicle.chassisBody.quaternion);
-    carro.roda1.quaternion.copy(vehicle.wheelBodies[0].quaternion);
-    carro.roda2.quaternion.copy(vehicle.wheelBodies[1].quaternion);
-    carro.roda3.quaternion.copy(vehicle.wheelBodies[2].quaternion);
-    carro.roda4.quaternion.copy(vehicle.wheelBodies[3].quaternion);
+    carro.attPositions();
 
     groundMesh.position.copy(groundBody.position);
     groundMesh.quaternion.copy(groundBody.quaternion);
