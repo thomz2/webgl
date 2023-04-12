9479b4388f42a521b98a0235e131f9e4573523ec
Thomaz <82160387+thomz2@users.noreply.github.com>
2023-04-12 20:29:22 -0300

    Create fisica.js

---
+++ src/js/fisica.js
@@ -0,0 +1,17 @@
+// fisica.js
+
+export function distPontos2D(x1, y1, x2, y2) {
+    return Math.sqrt( Math.pow( (x1 - x2), 2 ) + Math.pow( (y1 - y2), 2 ) );
+}
+
+export function dist3Pontos2D(x1, y1, z1, x2, y2, z2) {
+    return Math.sqrt( Math.pow( (x1 - x2), 2 ) + Math.pow( (y1 - y2), 2 ) + Math.pow( (z1 - z2), 2 ) ) ;
+}
+
+// ANGULO EM RADIANO
+export function calcular_posicao_vertice(centro_x, centro_y, raio, angulo) {
+    x = centro_x + raio * Math.cos(angulo);
+    y = centro_y + raio * Math.sin(angulo);
+
+    return {x, y};
+}
